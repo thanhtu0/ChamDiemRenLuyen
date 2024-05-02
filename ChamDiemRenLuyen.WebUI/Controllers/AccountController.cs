58e22ba7c5f6e42387003394d283cfb4f1c5c132
thanhtu0 <83869938+thanhtu0@users.noreply.github.com>
2024-05-02 14:35:18 +0700

    Update UI: Layout Teacher(Advirsor, Department Council, SchoolCouncil) and EvaluationForm.

---
+++ ChamDiemRenLuyen.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChamDiemRenLuyen.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// Giao diện trang đăng nhập
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
